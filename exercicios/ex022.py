3a852f5e958db055d0cf9eeb7932c8b231fdb043
Marcelo Rangel <156552374+marcelohar@users.noreply.github.com>
2024-07-07 21:54:43 -0400

    ex022

---
+++ exercicios/ex022.py
@@ -0,0 +1,20 @@
+# * Exercício 022
+#                 crie um programa que leia o nome completo com todas as letras:
+#                 - maiusculas
+#                 - minusculas
+#                 - qtd letras sem espaço
+#                 - qtd letras o primeiro nome
+
+print('\n{:=^40}\n'.format(' Desafio 022 '))
+
+nome = str(input('Qual seu nome: '))
+print('Maiúsculas: ', nome.upper())
+print('Minúscula: ', nome.lower())
+lNome = nome.split()
+jNome = ''.join(lNome)
+print('Qtd sem espaço: ', len(jNome))
+print('Qtd letras do primeiro nome:',len(lNome[0]))
+
+
+
+
