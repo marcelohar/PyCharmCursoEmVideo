ebb2a9f505b9ea1c9de08fef6ca172ec2893aa5c
Marcelo Rangel <marcelohar@hotmail.com>
2024-07-09 15:07:47 -0400

    Correcao-ex023

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -6,13 +6,34 @@
 #                     centena: 8
 #                     milhar: 1
 
+# Eu...
+"""
 print('\n {:=^40}'.format(' Desafio 023 '))
 
 n = str(input('Informe um número de até 4 digitos: '))
-
-
+n = '{:0>4}'.format(n)
+print(n)
 print('Unidade: {} \n'
       ' Dezena: {} \n'
       'Centena: {} \n'
       ' milhar: {}'.format(n[3], n[2], n[1], n[0]))
+"""
+
+# Professor...
+
+num = int(input('Informe um número: '))
+u = num // 1 % 10                           # num 'divisão inteira' por 1 e do resultado, faça o 'resto da divisão' entre o 'resultado' e '10'. trara o resto da divisão.
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print(' Analisando o número {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
+
+
+
+
 
