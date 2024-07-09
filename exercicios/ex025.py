3e4ceffaa3d214dc2f49c2435bf4941bf93b16a4
Marcelo Rangel <marcelohar@hotmail.com>
2024-07-09 15:47:23 -0400

    correcao-ex025

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,6 +1,9 @@
 #  * Exercício 025
 #                 Crie um programa que leia o nome de uma pessoa e diga se ela tem 'silva' no nome
 
+# Eu...
+
+"""
 print('\n{:=^40}'.format(' Desafio 025 '))
 
 nome = str(input('Nome: '))
@@ -12,3 +15,11 @@ if posNome == -1:
     print('Não tem o sobrenome "Silva"')
 else:
     print('Sobrenome "Silva" Encontrado na posição {}.'.format(posNome + 1))
+"""
+
+# Professor...
+nome = str(input('Qual é seu nome completo? ')).strip()             #.strip() remove os espaços iniciais e finais
+print('Seu nome tem silva? {}'.format('silva' in nome.lower()))     # 'silva' está em: nome '.lower' = em minúsculo? se sim é true
+
+
+
