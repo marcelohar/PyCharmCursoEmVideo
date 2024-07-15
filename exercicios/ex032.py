d3b88458e633d562bcf4b1861a9718cc1b2c839a
Marcelo Rangel <156552374+marcelohar@users.noreply.github.com>
2024-07-15 16:13:52 -0400

    correcao-ex032

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -25,4 +25,50 @@ else:
 
 '''
 
+# Professor
+from datetime import date                                                   # Trabalhar somente com datas
+
+
+ano = int(input("Que ano quer analisar? Coloque 0 para o ano atual: "))
+
+if ano == 0:
+    ano = date.today().year                                         # pega a data do dia atual
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:           # se o resto da divisão do ano por 4 = 0 'e' o resto da divisão do ano por 100 for diferente ou o resto da divisão do ano por 400 == 0 . só se ocorrer tudo isso..
+
+    print(f'O ano {ano} é bissexto')
+else:
+    print(f'O ano {ano} não é bissexto')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
