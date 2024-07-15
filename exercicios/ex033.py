eb0ecc347f7e6b0a04d506bcb9b5d4d1b970f571
Marcelo Rangel <156552374+marcelohar@users.noreply.github.com>
2024-07-15 16:35:28 -0400

    correcao-ex033

--- exercicios/ex033.py
+++ exercicios/ex033.py
@@ -1,6 +1,8 @@
 # * Exercício 033
 #         Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 
+#Eu... esqueci de de fazer qual era o maior e qual era o menor
+"""
 print('\n{:=^40}'.format(' Desafio 033 '))
 
 print('Informe 3 número')
@@ -8,7 +10,7 @@ sN1 = str(input('1º Nº: ')).strip()
 sN2 = str(input('2º Nº: ')).strip()
 sN3 = str(input('3º Nº: ')).strip()
 
-if sN1.isnumeric() and sN2.isnumeric() and sN3 and not sN1.isspace() and not sN2.isspace() and not sN3.isspace():
+if sN1.isnumeric() and sN2.isnumeric() and sN3:
     n1 = int(sN1)
     n2 = int(sN2)
     n3 = int(sN3)
@@ -22,3 +24,41 @@ if sN1.isnumeric() and sN2.isnumeric() and sN3 and not sN1.isspace() and not sN2
 
 else:
     print('[ERRO] Dados inválidos.')
+
+"""
+
+# Professor
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+print(f'Maior valor {maior}')
+print(f'Menor valor {menor}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
